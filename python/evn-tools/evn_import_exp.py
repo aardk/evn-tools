35e5d9ab5b0dc8f6a83ab1cd471afd12a695216d
Aard Keimpema <keimpema@jive.eu>
2021-07-13 22:36:02 +0000

    Add function to import fitsfiles + antab + uvflg for an experiment

---
+++ python/evn-tools/evn_import_exp.py
@@ -0,0 +1,45 @@
+import re
+import pathlib
+import pyvo
+from urllib.request import urlopen, urlretrieve
+from urllib.parse import urlparse
+
+service = pyvo.dal.TAPService('http://evn-vo.jive.eu/tap')
+
+def evn_import_exp(expname):
+    query = service.search(f"SELECT * FROM ivoa.obscore WHERE obs_id = '{expname}'")
+    dataobj = query[0].getdataobj()
+    urls = [f for f in dataobj.getcolumn('access_url').data]
+    if type(urls[0]) == bytes:
+        urls = [x.decode() for x in urls]
+
+    for url in urls:
+        filename = pathlib.Path(urlparse(url).path).name
+        urlretrieve(url, filename)
+
+    # Currently the calibration files are not in the VO, as work around try to get these from 
+    # the archive using some heuristics
+    with urlopen('http://archive.jive.nl/exp/') as response:
+        lines = response.read().decode('utf-8').split('\n')
+    exp = expname.upper()
+    r = re.compile(r'href=\"(\w+)_(\d+)')
+    dirlines = [i for i in lines if 'alt="[DIR]"' in i]
+    dirname = ""
+    for line in dirlines:
+        result = r.findall(line)
+        if (len(result) == 1) and (len(result[0]) == 2) and (result[0][0] == exp):
+            dirname = '_'.join(result[0])
+            break
+    if dirname == "":
+        print("WARNING: No calibration tables loaded from archive")
+        return
+    url = f'http://archive.jive.nl/exp/{dirname}/pipe'
+    with urlopen(url) as response:
+        lines = response.read().decode('utf-8').split('\n')
+    r = re.compile(r'href=\"([-+\w\.]*)\"')
+    for line in lines:
+        result = r.findall(line)
+        if len(result) == 1:
+            lowername = result[0].lower()
+            if lowername.endswith('.antab.gz') or lowername.endswith('.uvflg'):
+                urlretrieve(url + '/' + result[0], result[0])
